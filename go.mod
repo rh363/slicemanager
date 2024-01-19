a0ea7a81bc71cfc0b5ab36cea88d4fae8e0d6434
rh363 <alex.massaroni03@gmail.com>
2024-01-19 14:15:50 +0100

    new file:   go.mod 	new file:   slicemanager.go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rh363/slicemanager
+
+go 1.21.4
---
+++ slicemanager.go
@@ -0,0 +1,22 @@
+package slicemanager
+
+/*
+require string slice and index to remove
+*/
+func remove_string(slice []string, index int) []string {
+	return append(slice[:index], slice[index+1:]...) // it take content in slice until the index passed and append every content after the index to it, ... is used for pass element in second slice one by one
+}
+
+/*
+require string slice,index where insert string and the string to insert
+*/
+func insert_string(slice []string, index int, s string) []string {
+	var ns []string
+	for i, line := range slice {
+		ns = append(ns, line)
+		if i == index {
+			ns = append(ns, s)
+		}
+	}
+	return ns
+}
