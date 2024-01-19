b98aad84c81dd702ff6f3b4812e15e71fb9f993f
rh363 <alex.massaroni03@gmail.com>
2024-01-19 15:12:52 +0100

    modified:   slicemanager.go

--- slicemanager.go
+++ slicemanager.go
@@ -2,18 +2,23 @@ package slicemanager
 
 import "fmt"
 
+func Hello() {
+	fmt.Println("hello")
+}
+
 /*
+
 require string slice and index to remove
-*/
+
 
 func RemoveString(slice []string, index int) []string {
 	fmt.Println("done")
 	return append(slice[:index], slice[index+1:]...) // it take content in slice until the index passed and append every content after the index to it, ... is used for pass element in second slice one by one
 }
 
-/*
+
 require string slice,index where insert string and the string to insert
-*/
+
 func InsertString(slice []string, index int, s string) []string {
 	var ns []string
 	for i, line := range slice {
@@ -25,7 +30,7 @@ func InsertString(slice []string, index int, s string) []string {
 	return ns
 }
 
-/*
+
 func remove_string(slice []string, index int) []string {
 	return append(slice[:index], slice[index+1:]...) // it take content in slice until the index passed and append every content after the index to it, ... is used for pass element in second slice one by one
 }
@@ -41,4 +46,5 @@ func insert_string(slice []string, index int, s string) []string {
 	}
 	return ns
 }
+
 */
