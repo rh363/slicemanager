1bacff078508da90f4625efc3ddfc5943058f4a4
rh363 <alex.massaroni03@gmail.com>
2024-01-19 14:38:12 +0100

    modified:   slicemanager.go

--- slicemanager.go
+++ slicemanager.go
@@ -4,8 +4,12 @@ package slicemanager
 require string slice and index to remove
 */
 
-func remove_string(slice []string, index int) []string {
-	return append(slice[:index], slice[index+1:]...) // it take content in slice until the index passed and append every content after the index to it, ... is used for pass element in second slice one by one
+type sliceInit struct {
+	slice []string
+}
+
+func (Slice sliceInit) remove_string(index int) []string {
+	return append(Slice.slice[:index], Slice.slice[index+1:]...) // it take content in slice until the index passed and append every content after the index to it, ... is used for pass element in second slice one by one
 }
 
 /*
@@ -21,3 +25,21 @@ func insert_string(slice []string, index int, s string) []string {
 	}
 	return ns
 }
+
+/*
+func remove_string(slice []string, index int) []string {
+	return append(slice[:index], slice[index+1:]...) // it take content in slice until the index passed and append every content after the index to it, ... is used for pass element in second slice one by one
+}
+
+
+func insert_string(slice []string, index int, s string) []string {
+	var ns []string
+	for i, line := range slice {
+		ns = append(ns, line)
+		if i == index {
+			ns = append(ns, s)
+		}
+	}
+	return ns
+}
+*/
